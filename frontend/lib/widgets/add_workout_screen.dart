c9990b08fa9216afe42fcc1a98576e0838ca310f
Guillaume Ojardias <7831777+GuillaumeOj@users.noreply.github.com>
2024-03-02 13:17:04 +0100

    WOL-48/feat/Wraps round in workout form (#12)
    * improvement: makes sure optionsViewBuilder as the same widht as TextFormField
    
    This makes sures the option list build under the auto complete
    TextFormField as the sae width thant the field itself.
    
    * feat: wraps AddRound in AddWorkoutForm

--- frontend/lib/widgets/add_workout_screen.dart
+++ frontend/lib/widgets/add_workout_screen.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:wod_board_app/settings.dart';
-import 'package:wod_board_app/widgets/round/add_round.dart';
-
-import 'login_form.dart';
+import 'package:wod_board_app/widgets/add_workout_form.dart';
+import 'package:wod_board_app/widgets/login_form.dart';
 
 const Duration debounceDuration = Duration(milliseconds: 500);
 
@@ -26,11 +25,7 @@ class _AddWorkoutScreenState extends State<AddWorkoutScreen> {
           padding: const EdgeInsets.symmetric(horizontal: 20.0),
           child: currentUser.isAnonymous == true
               ? const LoginForm()
-              : const Column(
-                  children: [
-                    AddRound(),
-                  ],
-                ),
+              : const AddWorkoutForm(),
         ),
       ],
     );
