19d0c3c2f04a865e2f27ca339b3ba14fb8578d4c
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2024-03-08 10:45:28 +0100

    feat: adds Navigator to handle screens in the app

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -4,10 +4,8 @@ import "package:flutter/material.dart";
 import "package:flutter_dotenv/flutter_dotenv.dart";
 import "package:provider/provider.dart";
 import "package:wod_board_app/settings.dart";
-import "package:wod_board_app/widgets/workouts/create_workout_screen.dart";
 import "package:wod_board_app/widgets/bottom_bar.dart";
-import "package:wod_board_app/widgets/home_screen.dart";
-import "package:wod_board_app/widgets/profile_screen.dart";
+import "package:wod_board_app/widgets/routers.dart";
 
 Future main() async {
   await dotenv.load(mergeWith: Platform.environment);
@@ -26,30 +24,13 @@ class MyApp extends StatefulWidget {
 }
 
 class MyAppState extends State<MyApp> {
-  int currentIndex = 0;
-
-  final List<Map<String, dynamic>> staticScreens = const [
-    {
-      "icon": Icon(Icons.home),
-      "label": "Home",
-      "screen": HomeScreen(),
-    },
-    {
-      "icon": Icon(Icons.add),
-      "label": "Add Workout",
-      "screen": AddWorkoutScreen()
-    },
-    {
-      "icon": Icon(Icons.account_circle),
-      "label": "MyProfile",
-      "screen": ProfileScreen()
-    },
-  ];
+  final navigatorKey = GlobalKey<NavigatorState>();
+  String _currentScreenLabel = "Home";
 
   // Callback function to update currentIndex
-  void onTabTapped(int index) {
+  void onTabTapped(int index, String screenLabel) {
     setState(() {
-      currentIndex = index;
+      _currentScreenLabel = screenLabel;
     });
   }
 
@@ -62,12 +43,17 @@ class MyAppState extends State<MyApp> {
         useMaterial3: true,
       ),
       home: Scaffold(
-        appBar: AppBar(title: Text(staticScreens[currentIndex]["label"])),
-        body: staticScreens[currentIndex]["screen"],
+        appBar: AppBar(title: Text(_currentScreenLabel)),
+        body: Navigator(
+          key: navigatorKey,
+          initialRoute: Routes.home,
+          onGenerateRoute: (RouteSettings settings) {
+            return WorkoutRouter.generateRoute(settings);
+          },
+        ),
         bottomNavigationBar: BottomBar(
-          currentIndex: currentIndex,
-          itemValues: staticScreens,
           onTabTapped: onTabTapped,
+          navigatorKey: navigatorKey,
         ),
       ),
     );
--- frontend/lib/widgets/bottom_bar.dart
+++ frontend/lib/widgets/bottom_bar.dart
@@ -1,27 +1,54 @@
 import "package:flutter/material.dart";
+import "package:wod_board_app/widgets/routers.dart";
 
 class BottomBar extends StatefulWidget {
-  const BottomBar(
-      {super.key,
-      required this.currentIndex,
-      required this.itemValues,
-      required this.onTabTapped});
+  const BottomBar({
+    super.key,
+    required this.onTabTapped,
+    required this.navigatorKey,
+  });
 
-  final int currentIndex;
-  final List<Map<String, dynamic>> itemValues;
-  final void Function(int) onTabTapped;
+  final GlobalKey<NavigatorState> navigatorKey;
+  final void Function(int, String) onTabTapped;
 
   @override
   State<BottomBar> createState() => _BottomBarState();
 }
 
 class _BottomBarState extends State<BottomBar> {
+  int currentIndex = 0;
+  final List<Map<String, dynamic>> staticScreens = const [
+    {
+      "icon": Icon(Icons.home),
+      "label": "Home",
+      "route": Routes.home,
+    },
+    {
+      "icon": Icon(Icons.add),
+      "label": "Add Workout",
+      "route": Routes.createWorkout,
+    },
+    {
+      "icon": Icon(Icons.account_circle),
+      "label": "MyProfile",
+      "route": Routes.profile,
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
-      currentIndex: widget.currentIndex,
-      onTap: widget.onTabTapped,
-      items: widget.itemValues.map((item) {
+      currentIndex: currentIndex,
+      onTap: (int index) {
+        setState(() {
+          currentIndex = index;
+        });
+
+        widget.onTabTapped(index, staticScreens[index]["label"] as String);
+        widget.navigatorKey.currentState!
+            .pushNamed(staticScreens[index]["route"]);
+      },
+      items: staticScreens.map((item) {
         return BottomNavigationBarItem(
           icon: item["icon"] as Icon,
           label: item["label"] as String,
