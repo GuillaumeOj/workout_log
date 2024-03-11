33afb753baf6851a3fb5178d376b2bee12378949
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2024-03-11 17:48:32 +0100

    feat: adds endpoint to retieve user's workout details

--- backend/app/routers/workouts.py
+++ backend/app/routers/workouts.py
@@ -1,6 +1,7 @@
+import uuid as uuid_pkg
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlmodel import Session
 
 from app.crud.workout import create_rounds, get_workouts_by_user_id
@@ -57,3 +58,18 @@ async def get_workout_types():
     - **dict**: A dictionary with the available workout types under the key "values".
     """
     return {"values": [workout_type for workout_type in WorkoutType]}
+
+
+@router.get("/{workout_id}", response_model=WorkoutDetail, status_code=status.HTTP_200_OK)
+async def get_workout(
+    workout_id: uuid_pkg.UUID,
+    current_user: Annotated[User, Depends(get_current_user)],
+    session: Session = Depends(get_session),
+):
+    """Retrieve a specific workout by its ID if it belongs to the current_user."""
+    db_workout = session.get(Workout, workout_id)
+
+    if db_workout is None or db_workout.user_id != current_user.id:
+        raise HTTPException(status_code=404, detail="Workout not found")
+
+    return WorkoutDetail.from_db(db_workout)
