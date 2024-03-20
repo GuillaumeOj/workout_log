01606fa82dd964477a2b6fd65f22f734fffaf508
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2024-03-20 20:00:01 +0100

    feat: creates default user on stack init

--- backend/app/fixtures/users.py
+++ backend/app/fixtures/users.py
@@ -6,13 +6,17 @@ from app.crud.users import create_user as crud_create_user
 from app.models.users import User, UserCreate
 
 
-def create_user(session: Session, password: str | None = None) -> User:
+def create_user(session: Session, password: str | None = None, default_user: bool = False) -> User:
     user_password = password or "verycomplicatedpassworD-1"
 
     username_uuid = uuid.uuid4()
+
+    username = "foo" if default_user else f"foo-{username_uuid}"
+    email = f"{username}@bar.com"
+
     user = UserCreate(
-        username=f"foo-{username_uuid}",
-        email=f"foo-{username_uuid}@bar.com",
+        username=username,
+        email=email,
         password1=user_password,
         password2=user_password,
     )
